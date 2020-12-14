4e565a47123761e283bac1e301ce76ef85a89da4
unknown <mic7x4@gmail.com>
2020-12-14 10:19:26 +0200

    the first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+  const APP__ID = 'ab60b5ab';
+  const APP_KEY = '3c972572ac703b6e019ff31aa4afa0fd';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(()=>{
+    console.log('This is the worst Effect');
+    getRecipes();
+  },[]);
+
+const getRecipes = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP__ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  setRecipes(data.hits);
+
 }
+
+
+
+    return  (
+      <div className="App">
+        <form className="search__form" >
+          <input type="text" className="search__bar"/>
+          <button type="submit" className="search__button">Search</button>
+        </form>
+      
+      </div>
+
+    );
+}
+
 
 export default App;
